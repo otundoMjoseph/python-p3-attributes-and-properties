bdda1d47e89816b01b65f2709421e890f26e2521
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-05-29 20:46:52 +0300

    7 passed 8 failed to be continued

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+
+    def __init__(self, name='', breed=''):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            print("Name must be a string.")
+        elif not value:
+            print("Name must be a non-empty string.")
+        elif not (1 <= len(value) <= 25):
+            print("Name must be between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in self.APPROVED_BREEDS:
+            print("Breed must be one of the approved breeds.")
+        else:
+            self._breed = value
